9d01d499ee22fafe8ce1fbb6da5fdfbb07f5d980
Chee Ann Yap <victor.yap@siemens.com>
2019-09-10 12:44:36 +0800

    Testing API call

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('https://swapi.co/api/people/').subscribe(resData =>
+      console.log(resData.results.map(abc => abc.name)));
+  }
 
 }
